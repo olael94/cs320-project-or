7832763593071c2ada0044ef0777bee3e9d8b160
olael94 <104049815+olael94@users.noreply.github.com>
2024-09-20 02:51:55 -0600

    Worked on GreetingResource.java creating one new GET HTTP request and one new POST request. Tested successfully both requests with Postman.

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -1,16 +1,52 @@
 package org.acme;
 
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 
 @Path("/hello")
 public class GreetingResource {
 
-    @GET
+    @GET// this is a GET HTTP request
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         return "Hello RESTEasy";
     }
+
+    @Path("/personalized/{name}")
+    @GET// this is a GET HTTP request
+    @Produces(MediaType.TEXT_PLAIN)
+    public String personalizedHello(@PathParam("name") String name) {
+        return "Hello " + name;
+    }
+
+    @POST// this is a GET HTTP request
+    @Path("/personalized")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.TEXT_PLAIN)
+    public String personalizedHelloPost(Person p) {
+        return "Hello " + p.getFirstName() + " " + p.getLastName();
+    }
+
+    public static class Person {
+        private String firstName;
+        private String lastName;
+
+        public String getFirstName(){
+            return firstName;
+        }
+
+        public void setFirstName(String firstName) {
+            this.firstName = firstName;
+        }
+
+        public String getLastName(){
+            return lastName;
+        }
+
+        public void setLastName(String lastName) {
+            this.lastName = lastName;
+        }
+    }
+
 }
+
